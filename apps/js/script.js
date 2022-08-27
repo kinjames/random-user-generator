a7581cc3b3b7386fd5b3c8c7211b3596b1ddf9d4
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-08-27 21:24:26 +0000

    js update

--- apps/js/script.js
+++ apps/js/script.js
@@ -10,8 +10,6 @@ const showUser = async () => {
   // get user from api
   const person = await getUser();
   displayUser(person);
-
-  // display user
 };
 
 window.addEventListener('DOMContentLoaded', showUser);
