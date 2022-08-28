a59c26c571c91e3873b7fa65213588db4e542682
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-08-28 20:05:30 +0000

    script update

--- apps/js/script.js
+++ apps/js/script.js
@@ -1,5 +1,35 @@
 console.log("Hello World");
 
+const URL = 'https://randomuser.me/api/';
+
+const getUser = async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  // destructure
+  const person = data.results[0];
+  const { phone, email } = person;
+  const { large: image } = person.picture;
+  const { password } = person.login;
+  const { first, last } = person.name;
+  const {
+    dob: { age },
+  } = person;
+  const {
+    street: { number, name },
+  } = person.location;
+  return {
+    image,
+    phone,
+    email,
+    password,
+    age,
+    street: `${number} ${name}`,
+    name: `${first} ${last}`,
+  };
+};
+
+export default getUser;
+
 import get from './utils/getElement.js';
 import getUser from './utils/fetchUser.js';
 import displayUser from './utils/displayUser.js';
